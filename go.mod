289d255f5e8b770e81446a0428e246680a4778d1
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-12-08 22:17:36 +0300

    add go 1.17 and 1.16
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/rustatian/dl
 
-go 1.16
+go 1.17
--- go1.17.4/main.go
+++ go1.17.4/main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/rustatian/dl/internal/version"
 
 func main() {
-	version.Run("go1.17rc1")
+	version.Run("go1.17.4")
 }
